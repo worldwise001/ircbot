06fdca5fea40fe123b76e848db7fe864b691128f
Sarah Harvey <worldwise001@gmail.com>
2009-07-25 19:09:57 +0000

    final final fix

--- ircbotd/io.h
+++ ircbotd/io.h
@@ -1,8 +1,6 @@
 #ifndef IO_H_
 #define IO_H_
 
-#include "datatype.h"
-
 char * get_next_line(int fd);
 int get_next_char(int fd);
 int write_data(int fd, char * data);
--- ircbotd/socket.h
+++ ircbotd/socket.h
@@ -1,6 +1,7 @@
 #ifndef SOCKET_H_
 #define SOCKET_H_
 
+#include "ircfunc.h"
 #include "config.h"
 #include "lib.h"
 
