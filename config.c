2f8ebb25960f33aa1dc814fe97c636329faa9492
worldwise001 <worldwise001@697919cb-dacd-4c6d-9bb0-887b5332b243>
2009-08-15 17:05:22 +0000

    Moved final 0.7.1 version

---
+++ config.c
@@ -0,0 +1,395 @@
+#include "config.h"
+
+extern globals_t globals;
+
+unsigned int is_value(char * field, char * type)
+{
+	return strncasecmp(field, type, strlen(type)) == 0;
+}
+
+llist_t * load_irccfg(char * filename)
+{
+	if (filename == NULL) filename = "ircbotd.conf";
+
+	irccfg_t d_irccfg, * i_irccfg = NULL;
+	llist_t * first = NULL, * iterator = NULL;
+	memset(&d_irccfg, 0, sizeof(irccfg_t));
+	int irccfg_fd = open(filename, O_RDONLY);
+	if (irccfg_fd == -1)
+	{
+		irc_printf(IRCERR, "Error opening config file %s: %s\n", filename, strerror(errno));
+		return NULL;
+	}
+
+	char * buff, *istr, *iend, *vstr, *vend, strid[6];
+	int line = 0;
+
+	while ((buff = get_next_line(irccfg_fd)) != NULL && ++line)
+	{
+		iterator = first;
+		istr = NULL;
+		iend = NULL;
+		vstr = NULL;
+		vend = NULL;
+		memset(strid, 0, 6);
+		if (buff[0] == '#' || strlen(buff) == 0)
+		{
+			free(buff);
+			continue;
+		}
+		if ((vstr = index(buff, '"')) == NULL)
+		{
+			irc_printf(IRCERR, "Invalid format \" on line %d\n", line);
+			free(buff);
+			continue;
+		}
+		vstr++;
+		if ((vend = rindex(buff, '"')) == (vstr-1))
+		{
+			irc_printf(IRCERR, "Invalid format \" on line %d\n", line);
+			free(buff);
+			continue;
+		}
+		if ((istr = index(buff, '[')) == NULL)
+			iend = NULL;
+		else
+		{
+			istr++;
+			iend = rindex(buff, ']');
+		}
+		if (istr != NULL && iend == NULL)
+		{
+			irc_printf(IRCERR, "Invalid format [] on line %d\n", line);
+			free(buff);
+			continue;
+		}
+		else if (istr != NULL)
+		{
+			strncpy(strid, istr, iend-istr);
+			strid[iend-istr] = '\0';
+		}
+		
+		if (strid[0] != '\0')
+		{
+			iterator = first;
+			while (iterator != NULL)
+			{
+				i_irccfg = (irccfg_t *)(iterator->item);
+				if (i_irccfg->id == atoi(strid))
+					break;
+				iterator = iterator->next;
+			}
+			if (iterator == NULL)
+			{
+				i_irccfg = malloc(sizeof(irccfg_t));
+				if (i_irccfg == NULL)
+				{
+					irc_printf(IRCERR, "Unable to create configuration block: %s\n", strerror(errno));
+					free(buff);
+					continue;
+				}
+				memset(i_irccfg, 0, sizeof(irccfg_t));
+				llist_t * result = append_item(first, i_irccfg);
+				if (result == NULL)
+				{
+					irc_printf(IRCERR, "Unable to add configuration block: %s\n", strerror(errno));
+					free(i_irccfg);
+					free(buff);
+					continue;
+				}
+				else
+					first = result;
+				i_irccfg->id = atoi(strid);
+			}
+		}
+		
+		int v_len = vend-vstr;
+		if (is_value(buff, "NICK"))
+		{
+			if (strlen(strid) > 0)
+				strncpy(i_irccfg->nick, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			else
+				strncpy(d_irccfg.nick, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			free(buff);
+			continue;
+		}
+		if (is_value(buff, "USER"))
+		{
+			if (strlen(strid) > 0)
+				strncpy(i_irccfg->user, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			else
+				strncpy(d_irccfg.user, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			free(buff);
+			continue;
+		}
+		if (is_value(buff, "REAL"))
+		{
+			if (strlen(strid) > 0)
+				strncpy(i_irccfg->real, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			else
+				strncpy(d_irccfg.real, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			free(buff);
+			continue;
+		}
+		if (is_value(buff, "PASS"))
+		{
+			if (strlen(strid) > 0)
+				strncpy(i_irccfg->pass, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			else
+				strncpy(d_irccfg.pass, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			free(buff);
+			continue;
+		}
+		if (is_value(buff, "HOST"))
+		{
+			if (strlen(strid) > 0)
+				strncpy(i_irccfg->host, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			else
+				strncpy(d_irccfg.host, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			free(buff);
+			continue;
+		}
+		if (is_value(buff, "PORT"))
+		{
+			char sport[6];
+			memset(sport, 0, 6);
+			strncpy(sport, vstr, (v_len > 5)?5:v_len);
+			if (strlen(strid) > 0)
+				i_irccfg->port = atoi(sport);
+			else
+				d_irccfg.port = atoi(sport);
+			free(buff);
+			continue;
+		}
+		if (is_value(buff, "CHAN"))
+		{
+			if (strlen(strid) > 0)
+				strncpy(i_irccfg->chan, vstr, (v_len > CFG_FLD*8)?CFG_FLD*8:v_len);
+			else
+				strncpy(d_irccfg.chan, vstr, (v_len > CFG_FLD*8)?CFG_FLD*8:v_len);
+			free(buff);
+			continue;
+		}
+		if (is_value(buff, "AUTH"))
+		{
+			if (strlen(strid) > 0)
+				strncpy(i_irccfg->auth, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			else
+				strncpy(d_irccfg.auth, vstr, (v_len > CFG_FLD)?CFG_FLD:v_len);
+			free(buff);
+			continue;
+		}
+		if (is_value(buff, "SOCKET"))
+		{
+			char sbool[6];
+			memset(sbool, 0, 6);
+			strncpy(sbool, vstr, (v_len > 5)?5:v_len);
+			if (strlen(strid) > 0)
+				i_irccfg->enabled = (strncmp("true", sbool, 4) == 0);
+			else
+				d_irccfg.enabled = (strncmp("true", sbool, 4) == 0);
+			free(buff);
+			continue;
+		}
+	}
+
+	iterator = first;
+	while (iterator != NULL)
+	{
+		i_irccfg = (irccfg_t *)(iterator->item);
+		if (strlen(i_irccfg->nick) == 0)
+			strncpy(i_irccfg->nick, d_irccfg.nick, CFG_FLD);
+		if (strlen(i_irccfg->user) == 0)
+			strncpy(i_irccfg->user, d_irccfg.user, CFG_FLD);
+		if (strlen(i_irccfg->real) == 0)
+			strncpy(i_irccfg->real, d_irccfg.real, CFG_FLD);
+		if (strlen(i_irccfg->pass) == 0)
+			strncpy(i_irccfg->pass, d_irccfg.pass, CFG_FLD);
+		if (strlen(i_irccfg->chan) == 0)
+			strncpy(i_irccfg->chan, d_irccfg.chan, CFG_FLD*8);
+		if (strlen(i_irccfg->auth) == 0)
+			strncpy(i_irccfg->auth, d_irccfg.auth, CFG_FLD);
+		if (strlen(i_irccfg->host) == 0)
+			strncpy(i_irccfg->host, d_irccfg.host, CFG_FLD);
+		if (i_irccfg->port == 0)
+			i_irccfg->port = d_irccfg.port;
+		if (i_irccfg->enabled == 0)
+			i_irccfg->enabled = d_irccfg.enabled;
+		iterator = iterator->next;
+	}
+	
+	print_irccfg(first);
+	
+	return first;
+}
+
+void print_irccfg(llist_t * irclist)
+{
+	llist_t * iterator = irclist;
+	while (iterator != NULL)
+	{
+		irccfg_t * m_irccfg = (irccfg_t *)(iterator->item);
+		irc_printf(IRCERR, "%d \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" %s:%d\n", m_irccfg->enabled, m_irccfg->nick, m_irccfg->user, m_irccfg->real, m_irccfg->pass, m_irccfg->chan, m_irccfg->auth, m_irccfg->host, m_irccfg->port);
+		iterator = iterator->next;
+	}	
+}
+
+int load_args(int argc, char** argv, args_t * args)
+{
+	int i = -1;
+	int expected_args = 0;
+	while (++i < argc)
+	{
+		if (argv[i][0] != '-' && expected_args == 0)
+		{
+			irc_printf(IRCERR, "Invalid flag: %s\n", argv[i]);
+			return -1;
+		}
+		else if (expected_args > 0)
+		{
+
+			if (args->conf_file == NULL)
+				args->conf_file = argv[i];
+			expected_args--;
+		}
+		else if (argv[i][0] == '-')
+		{
+			char * buff = argv[i];
+			if (*(buff+1) == '\0')
+			{
+				irc_printf(IRCERR, "Invalid flag: %s\n", argv[i]);
+				return -1;
+			}
+			while (*(++buff) != '\0')
+			{
+				switch (*buff)
+				{
+				case 'v': (args->verbose)++; break;
+				case 'd': args->daemon = 1; break;
+				case 'c': expected_args++; break;
+				case 'V': args->version++; break;
+				case 'h': args->help++; break;
+				case 'l': args->log = 1; break;
+				case 'r': args->raw = 1; break;
+				case '-':
+					if (strcmp(&buff[1], "daemon") == 0)
+						args->daemon = 1;
+					else if (strcmp(&buff[1], "config") == 0)
+						expected_args++;
+					else if (strcmp(&buff[1], "version") == 0)
+						args->version++;
+					else if (strcmp(&buff[1], "help") == 0)
+						args->help++;
+					else if (strcmp(&buff[1], "log") == 0)
+						args->log = 1;
+					else if (strcmp(&buff[1], "raw") == 0)
+						args->raw = 1;
+					else
+					{
+						irc_printf(IRCERR, "Invalid flag: %s\n", argv[i]);
+						return -1;
+					}
+					buff+=(strlen(argv[i])-2);
+					break;
+				default:
+					irc_printf(IRCERR, "Invalid flag: %c\n", *buff);
+					return -1;
+				}
+			}
+			continue;
+		}
+
+	}
+	if (expected_args > 0)
+	{
+		irc_printf(IRCERR, "Expecting an argument; perhaps you forgot the configuration file?\n");
+		return -1;
+	}
+	return 0;
+}
+
+void open_log()
+{
+	if (globals._log)
+	{
+		char * filename = NULL;
+		if (getpid() == globals.parent_pid)
+		{
+			filename = malloc(11+strlen(LOGDIR));
+			memset(filename, 0, 11+strlen(LOGDIR));
+			sprintf(filename, "%s/circe.log", LOGDIR);
+		}
+		else if (getpid() != globals.lib_pid)
+		{
+			filename = malloc(11+strlen(LOGDIR)+strlen(globals.m_irccfg.host));
+			memset(filename, 0, 11+strlen(LOGDIR)+strlen(globals.m_irccfg.host));
+			sprintf(filename, "%s/%i-%s.log", LOGDIR, globals.m_irccfg.id, globals.m_irccfg.host);
+		}
+		else
+		{
+			filename = malloc(9+strlen(LOGDIR));
+			memset(filename, 0, 9+strlen(LOGDIR));
+			sprintf(filename, "%s/lib.log", LOGDIR);
+		}
+		globals._ircout = fopen(filename, "a");
+		free(filename);
+		filename = NULL;
+		if (globals._ircout == NULL) return;
+		filename = malloc(11+strlen(LOGDIR));
+		memset(filename, 0, 11+strlen(LOGDIR));
+		sprintf(filename, "%s/error.log", LOGDIR);
+		globals._ircerr = fopen(filename, "a");
+		free(filename);
+		filename = NULL;
+		if (globals._ircerr == NULL) return;
+	}
+}
+
+void close_log()
+{
+	if (globals._log)
+	{
+		fclose(globals._ircout);
+		fclose(globals._ircerr);
+		globals._ircout = NULL;
+		globals._ircerr = NULL;
+	}
+}
+
+void open_raw()
+{
+	if (globals._raw)
+	{
+		char *filename = malloc(15+strlen(LOGDIR)+strlen(globals.m_irccfg.host));
+		memset(filename, 0, 15+strlen(LOGDIR)+strlen(globals.m_irccfg.host));
+		sprintf(filename, "%s/raw-%i-%s.log", LOGDIR, globals.m_irccfg.id, globals.m_irccfg.host);
+		globals._ircraw = fopen(filename, "a");
+		free(filename);
+		filename = NULL;
+		if (globals._ircraw == NULL) return;
+	}
+}
+
+void close_raw()
+{
+	if (globals._raw)
+	{
+		fclose(globals._ircraw);
+		globals._ircraw = NULL;
+	}
+}
+
+int get_by_pid(llist_t * first, pid_t pid)
+{
+	if (first == NULL) return -1;
+	llist_t * iterator = first;
+	int i = 0;
+	while (iterator != NULL)
+	{
+		irccfg_t * i_irccfg = (irccfg_t *)(iterator->item);
+		if (i_irccfg->pid == pid) return i;
+		iterator = iterator->next;
+		i++;
+	}
+	return -1;
+}
---
+++ socket.c
@@ -0,0 +1,170 @@
+#include "socket.h"
+
+extern globals_t globals;
+
+int sock_connect(char* host, int port)
+{
+	struct addrinfo hints, *result, *res_ptr;
+	int sfd, ai_res;
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+
+	char sport[6];
+	snprintf(sport, 6, "%d", port);
+
+	switch((ai_res = getaddrinfo(host, sport, &hints, &result)))
+	{
+	case 0: break;
+	default:
+		irc_printf(IRCERR, "getaddrinfo failed: %s\n", gai_strerror(ai_res));
+		return -1;
+		break;
+	}
+
+	res_ptr = result;
+	do
+	{
+		if ((sfd = socket(res_ptr->ai_family, res_ptr->ai_socktype, res_ptr->ai_protocol)) != -1)
+		{
+			if (connect(sfd, res_ptr->ai_addr, res_ptr->ai_addrlen) == 0)
+				break;
+			close(sfd);
+		}
+
+	} while ((res_ptr = res_ptr->ai_next) != NULL);
+
+	freeaddrinfo(result);
+	if (res_ptr == NULL)
+	{
+		irc_printf(IRCERR, "connect failed\n");
+		return -1;
+	}
+	return sfd;
+}
+
+int sock_handshake(irccfg_t * m_irccfg)
+{
+	char * line  = NULL;
+
+	write_data(m_irccfg->sfd, "\n");
+	write_data(m_irccfg->sfd, "NICK ");
+	write_data(m_irccfg->sfd, m_irccfg->nick);
+	write_data(m_irccfg->sfd, "\n");
+	if (errno)
+	{
+		irc_printf(IRCERR, "Error writing to stream: %s\n", strerror(errno));
+		close(m_irccfg->sfd);
+		return -1;
+	}
+	write_data(m_irccfg->sfd, "USER ");
+	write_data(m_irccfg->sfd, m_irccfg->user);
+	write_data(m_irccfg->sfd, " * * :");
+	write_data(m_irccfg->sfd, m_irccfg->real);
+	write_data(m_irccfg->sfd, "\n");
+	if (errno)
+	{
+		irc_printf(IRCERR, "Error writing to stream: %s\n", strerror(errno));
+		close(m_irccfg->sfd);
+		return -1;
+	}
+
+	while (TRUE)
+	{
+		line = get_next_line(m_irccfg->sfd);
+		if (line == NULL)
+		{
+			irc_printf(IRCERR, "Error reading from socket\n");
+			close(m_irccfg->sfd);
+			return -1;
+		}
+		else if (strstr(line, " NOTICE AUTH :") != NULL)
+		{
+			free(line);
+			continue;
+		}
+		else if (strstr(line, "PING :") != NULL)
+		{
+			write_data(m_irccfg->sfd, "PONG :");
+			write_data(m_irccfg->sfd, &line[6]);
+			write_data(m_irccfg->sfd, "\n");
+			free(line);
+			continue;
+		}
+		else
+		{
+			if (strlen(m_irccfg->pass) > 0)
+				identify(m_irccfg);
+			if (strlen(m_irccfg->chan) > 0)
+				autojoin(m_irccfg);
+			process_input(m_irccfg, line);
+			break;
+		}
+	}
+	return 0;
+}
+
+void handle_conn(irccfg_t * m_irccfg)
+{
+	char * line = NULL;
+	while (TRUE)
+	{
+		line = get_next_line(m_irccfg->sfd);
+		if (line == NULL || strlen(line) == 0)
+		{
+			if (line == NULL) break;
+			if (line != NULL)
+			{
+			free(line);
+				line = NULL;
+			}
+			continue;
+		}
+		
+		if (strstr(line, "PING :") != NULL)
+		{
+			write_data(m_irccfg->sfd, "PONG :");
+			write_data(m_irccfg->sfd, &line[6]);
+			write_data(m_irccfg->sfd, "\n");
+			free(line);
+			line = NULL;
+		}
+		else
+			process_input(m_irccfg, line);
+	}
+}
+
+int autojoin(irccfg_t * m_irccfg)
+{
+	int res = 0;
+	char chancpy[CFG_FLD*8+1];
+		strcpy(chancpy, m_irccfg->chan);
+	if (chancpy != NULL)
+	{
+		char * chanptr = chancpy + strlen(chancpy) + 1;
+		while (chanptr > chancpy)
+		{
+			while (*(--chanptr) != ' ' && chanptr > chancpy);
+			if (chanptr > chancpy) chanptr++;
+			write_data(m_irccfg->sfd, "JOIN ");
+			write_data(m_irccfg->sfd, chanptr);
+			write_data(m_irccfg->sfd, "\n");
+			if (chanptr > chancpy) chanptr--;
+			if (*chanptr == ' ') *chanptr = '\0';
+		}
+	}
+	else res = -1;
+	return res;
+}
+
+int identify(irccfg_t * m_irccfg)
+{
+	int res = 0;
+	res += write_data(m_irccfg->sfd, "NICKSERV IDENTIFY ");
+	res += write_data(m_irccfg->sfd, m_irccfg->pass);
+	res += write_data(m_irccfg->sfd, "\n");
+	return res;
+}
+
+
