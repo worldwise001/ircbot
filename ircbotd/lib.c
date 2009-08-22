32644de69f186109772453a5048c14660fead361
Sarah Harvey <worldwise001@gmail.com>
2009-08-22 21:36:10 +0000

    Fixed memleaks; bot should quit cleanly now

--- ircbotd/lib.c
+++ ircbotd/lib.c
@@ -14,7 +14,7 @@ pthread_t set_up_lib_thread()
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-	int return_value = pthread_create(&tid, NULL, &lib_loop, NULL);
+	int return_value = pthread_create(&tid, &attr, &lib_loop, NULL);
 	if (return_value)
 		irc_printf(IRCERR, "Creating the library thread returned error code %d\n", return_value);
 	else
