578f601fc9b1443c6a0e42489c52ebf56e1f9727
Sarah Harvey <worldwise001@gmail.com>
2009-09-16 21:08:36 +0000

    Fixed modules to work with version 0.8

--- 0.8/ircbotd-modules/seen/seen.h
+++ 0.8/ircbotd-modules/seen/seen.h
@@ -12,18 +12,16 @@
 typedef struct {
 	msg_t msg;
 	time_t time;
-	pid_t pid;
+	pthread_t tid;
 } seen_t;
 
 void parse(irccfg_t * info, msg_t * data);
 void commands(char * string);
 void name(char * string);
 
-void add_event(pid_t pid, msg_t * msg);
+void add_event(pthread_t tid, msg_t * msg);
 void find_target_last(irccfg_t * info, char * nick, char * target);
 void find_target_seen(irccfg_t * info, char * nick, char * target);
-seen_t * been_seen(pid_t pid, char * sender);
-
-field_t get_kicked_nick(char * message);
+seen_t * been_seen(pthread_t tid, char * sender);
 
 #endif
