41e66c790be47d6855b1b422ef18f8018160c45e
Sarah Harvey <worldwise001@gmail.com>
2010-05-26 21:46:59 +0000

    updated rejoin.c added Makefile

--- ircbotd_mod/mod_rejoin/rejoin.c
+++ ircbotd_mod/mod_rejoin/rejoin.c
@@ -1,13 +1,13 @@
-#include "../../module.h"
+#include "../../circebot/ircfunc.h"
 #include <string.h>
 
-void parse(info_t * config, msg_t * data)
+void parse(irccfg_t * config, msg_t * data)
 {
 	if (data->command != NULL && strncasecmp(data->command, "KICK", 4) == 0)
 	{
 		if (data->message != NULL)
 		{
-			if (strncasecmp(data->message, config->nickname, strlen(config->nickname)) == 0 && strncasecmp(data->message+strlen(config->nickname), " :", 2) == 0)
+			if (strncasecmp(data->message, config->nick, strlen(config->nick)) == 0 && strncasecmp(data->message+strlen(config->nick), " :", 2) == 0)
 				respond(config, "JOIN %s\n", data->target);
 		}
 	}
