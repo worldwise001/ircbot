febb2bacb0646952bd786690425eb6b519787373
Sarah Harvey <worldwise001@gmail.com>
2010-05-27 00:40:50 +0000

    Added licensing information

--- ircbotd/ircfunc.c
+++ ircbotd/ircfunc.c
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2009-2010 Sarah Harvey
+ *
+ * This file is part of CirceBot.
+ *
+ *  CirceBot is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CirceBot is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CirceBot.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include "ircfunc.h"
 
 globals_t globals;
@@ -21,8 +41,8 @@ void print_usage(char * app_name)
 
 void print_version(char * app_name)
 {
-	printf("CirceBot %s\n", VERSION);
-	printf("Written in C by worldwise001\n");
+	printf("CirceBot %s: IRC Bot written in C\n", VERSION);
+	printf("Copyright (C) 2010 Sarah Harvey\n");
 	printf("http://circebot.sourceforge.net\n");
 }
 
