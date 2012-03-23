eceb8bd8619962d9993fb0c3aeeb0f13c1506f6a
Sarah Harvey <worldwise001@gmail.com>
2012-03-22 22:03:59 -0400

    removed the comments

--- libcircle/circle.h
+++ libcircle/circle.h
@@ -1,23 +1,3 @@
-/*
- * Copyright 2009-2010 Sarah Harvey
- *
- * This file is part of Circle <http://circle.sourceforge.net>.
- *
- *  Circle is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Circle is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Circle.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-
 #ifndef _CIRCLE_H
 #define	_CIRCLE_H
 
--- libcircle/ircfunc.c
+++ libcircle/ircfunc.c
@@ -1,22 +1,3 @@
-/*
- * Copyright 2009-2010 Sarah Harvey
- *
- * This file is part of Circle <http://circle.sourceforge.net>.
- *
- *  Circle is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Circle is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Circle.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "ircfunc.h"
 
 void __circle_set_field(char * dest, char * src, int maxlen) {
